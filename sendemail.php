1018a09889242f3bfa649f7355353455564e51cf
kawakuti code <kawakuticode@gmail.com>
2016-11-10 05:51:33 +0100

    comit of end of the day

--- sendemail.php
+++ sendemail.php
@@ -11,7 +11,7 @@ $subject = @trim(stripslashes($_POST['subject']));
 $message = @trim(stripslashes($_POST['message']));
 
 $email_from = $email;
-$email_to = 'djrusselius@gmail.com';//replace with your email
+$email_to = 'telcosmsdevelopment@gmail.com';//replace with your email
 
 $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
